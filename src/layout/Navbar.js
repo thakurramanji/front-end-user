041cc6787578cef6dd38ad7b326aea5a4e778be4
unknown <thakurramanji@gmail.com>
2024-03-27 11:44:33 +0530

    view user added

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -7,9 +7,9 @@ export default function Navbar() {
 
 <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">
+    <Link className="navbar-brand" to={"/"}>
         FullStack Application 
-    </a>
+    </Link>
     <button className="navbar-toggler" 
         type="button" data-bs-toggle="collapse" 
         data-bs-target="#navbarSupportedContent" 
